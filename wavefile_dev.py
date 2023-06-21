3994f82a6cbe0b249d0d863af2cdb5e45c4d8fdb
brienalkire <97640022+brienalkire@users.noreply.github.com>
2023-06-21 16:11:22 -0700

    This version has been V&V'd for compatibility with 32-bit floating point format wavefiles.
    I generated a 32-bit floating point format wavefile of a mid-side guitar recording in Pro Tools. I V&V'd that the code can properly read the file, do the mid-side processing and write the results to output files in the correct format. I opened the results in Pro Tools and verified that the processing is correct (e.g., the two side channels cancel when panned together) and that the audio sounds consistent with a mid-side processed version generated entirely in Pro Tools.  I also tested the rumble filter in 32-bit float format and it seems to work fine in terms of the file I/O and file format, though I didn't conduct an audio test to ensure the filter is implemented.

--- wavefile_dev.py
+++ wavefile_dev.py
@@ -13,30 +13,49 @@ import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
 
-mono_filename='.\TrackWithRumble.wav'
-stereo_filename='.\CarmenSeguidillia_V16.wav'
+import sys   
+import logging
+ 
+try:
+    stereo_filename='.\garrotin_32bitfloat.wav'
+    
+    wf_stereo=wt.AudioFile()
+    wf_stereo.read_wavefile(stereo_filename) 
+    print('Num channels=',wf_stereo.m_numchannels)
+    print('Sample rate (Hz)=',wf_stereo.m_samplerate_Hz)
+    print('Num samples=',wf_stereo.m_numsamples)
+    print('Duration (seconds)=',wf_stereo.m_duration_s)
+    print('Type=',wf_stereo.m_data.dtype)
+  
+    sys.exit(0)
 
-wf_stereo=wt.AudioFile()
-wf_mono=wt.AudioFile()
-wf_stereo.read_wavefile(stereo_filename)
-wf_mono.read_wavefile(mono_filename)
-#mydata=np.array((wf_mono.m_data,wf_mono.m_data),dtype=np.int16).transpose()
-
-Filter1=wt.AudioFilter()
-Filter1.m_passbandedgefrequency_Hz=40
-Filter1.m_stopbandedgefrequency_Hz=20
-Filter1.design_rumble_filter(wf_stereo.m_samplerate_Hz)
-
-#Filter1.apply_filter(wf_stereo.m_data)
-input_data=wf_stereo.m_data
-output_data=input_data
-
-if 1 == len(input_data.shape):
-    output_data=signal.lfilter(Filter1.m_b,Filter1.m_a,input_data)
-else:
-    output_data[:,0]=signal.lfilter(Filter1.m_b,Filter1.m_a,input_data[:,0])
-    output_data[:,1]=signal.lfilter(Filter1.m_b,Filter1.m_a,input_data[:,1])
-
-
+    ############
+    mono_filename='.\TrackWithRumble.wav'
+    stereo_filename='.\CarmenSeguidillia_V16.wav'
+    
+    wf_stereo=wt.AudioFile()
+    wf_mono=wt.AudioFile()
+    wf_stereo.read_wavefile(stereo_filename)
+    wf_mono.read_wavefile(mono_filename)
+    #mydata=np.array((wf_mono.m_data,wf_mono.m_data),dtype=np.int16).transpose()
+    
+    Filter1=wt.AudioFilter()
+    Filter1.m_passbandedgefrequency_Hz=40
+    Filter1.m_stopbandedgefrequency_Hz=20
+    Filter1.design_rumble_filter(wf_stereo.m_samplerate_Hz)
+    
+    #Filter1.apply_filter(wf_stereo.m_data)
+    input_data=wf_stereo.m_data
+    output_data=input_data
+    
+    if 1 == len(input_data.shape):
+        output_data=signal.lfilter(Filter1.m_b,Filter1.m_a,input_data)
+    else:
+        output_data[:,0]=signal.lfilter(Filter1.m_b,Filter1.m_a,input_data[:,0])
+        output_data[:,1]=signal.lfilter(Filter1.m_b,Filter1.m_a,input_data[:,1])
+    #############    
+except Exception as e:
+    logging.warning(e)
+    logging.error(e)
 
 
